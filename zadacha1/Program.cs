eac36974b42f9d4ee4b8a4bbbcd78f60042fabea
Tatiana <tanja_prud@mail.ru>
2022-11-29 15:25:31 +0300

    Added zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+string[] array = new string[6] {"456", "Hello", "Kazan", "12", "num", "=!"};
+Console.Write($"Изходный массив: [");
+PrintArray(array);
+Console.Write($"] ");
+Console.Write($"\nИтоговый массив: [");
+string[] myArray = new string[array.Length];
+SortArray(array, myArray);
+PrintArray(myArray);
+Console.Write($"]");
+
+
+void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void SortArray(string[] array,string[] myArray)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        myArray[count] = array[i];
+        count++;
+    }
+}
